ff61670571d00b0127122d136b0dc88888b9b4c9
Darwin Asus <d.f.winzi2006@gmail.com>
2023-09-27 04:13:20 +0700

    fix: dashboard diagram data for user with most surat

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -21,6 +21,7 @@ class DashboardController extends Controller
         return $retVal;
     }
     /* ---------------------------------- VIEWS --------------------------------- */
+    // ! NEED REFACTOR. app still do unneccessary queries upon different role
     public function index()
     {
         $years = Carbon::now();
@@ -29,18 +30,18 @@ class DashboardController extends Controller
         foreach ($years_diagram as $year) {
             $all_counts[] = Surat::query()->where(DB::raw('YEAR(tanggal_surat)'), $year)->get()->count();
         }
-
-        // ! NEED REFACTOR. app still do unneccessary queries upon different role
+        $diagram = User::with('surat')
+            ->has('surat')
+            ->withCount('surat')->orderByDesc('surat_count')
+            ->take(10)->get(['username']);
         $data = [
             'surat_terbaru'      => Surat::query()->take(5)->get(['ringkasan', 'file', 'tanggal_surat']),
             'surat_data_diagram' => $all_counts,
             'tahun_diagram'      => $years_diagram,
-            'user_diagram'       => User::query()->take(10)->has('surat', '>=', 5)->get('username')->pluck('username'),
-            'user_data_diagram'  => User::query()->take(10)->has('surat', '>=', 5)->get('id')->map(function (User $user) {
-                return Surat::with('user')->where('id_user', $user->id)->get()->count();
-            }),
+            'user_diagram'       => $diagram->pluck('username'),
+            'user_data_diagram'  => $diagram->pluck("surat_count"),
         ];
-        return JenisSurat::with('surat')->get(); //!todo count on most popular
+        // return JenisSurat::with('surat')->get(); //!todo count on most popular
         return view('dashboard.index', $data);
     }
 }
