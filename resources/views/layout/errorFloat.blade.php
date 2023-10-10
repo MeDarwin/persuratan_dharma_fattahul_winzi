de00d75185a5b3df402976d1171c389676d0959c
Darwin Asus <d.f.winzi2006@gmail.com>
2023-10-10 13:30:27 +0700

    refactor: only show error float on any errors

--- resources/views/layout/errorFloat.blade.php
+++ resources/views/layout/errorFloat.blade.php
@@ -1,5 +1,5 @@
-<div class="position-absolute top-50 start-50 translate-middle z-3 w-75">
-    @if ($errors->any())
+@if ($errors->any())
+    <div class="position-absolute top-50 start-50 translate-middle z-3 w-75">
         <div class="alert alert-danger alert-dismissible">
             <h2 class="alert-heading border-bottom">Gagal!</h2>
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
@@ -7,5 +7,5 @@
                 <p class="m-0 p-0"> {{ $message }}</p>
             @endforeach
         </div>
-    @endif
-</div>
+    </div>
+@endif
